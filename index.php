eb19317bd3ab74a8436c3537745dd75baaf9a2d5
confusingSmile <waltervdberg91@hotmail.com>
2015-06-16 16:46:34 +0200

    changed hrml class-name

--- index.php
+++ index.php
@@ -31,68 +31,68 @@ and open the template in the editor.
             <table class="hoofdmenuTable">
                 <tr>
                     <td>
-                        <button onclick="location.href='registreerBieder.php'" class="optionButton">
+                        <button onclick="location.href='registreerBieder.php'" class="hoofdmenuOptionButton">
                             Nieuwe bieder
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='registreerAanbieder.php'" class="optionButton">
+                        <button onclick="location.href='registreerAanbieder.php'" class="hoofdmenuOptionButton">
                             Nieuwe aanbieder
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='klantenregister.php'" class="optionButton">
+                        <button onclick="location.href='klantenregister.php'" class="hoofdmenuOptionButton">
                             klantenregister
                         </button>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <button onclick="location.href='registreerGoederen.php'" class="optionButton">
+                        <button onclick="location.href='registreerGoederen.php'" class="hoofdmenuOptionButton">
                             Goederen registreren
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='verkavelGoederen.php'" class="optionButton">
+                        <button onclick="location.href='verkavelGoederen.php'" class="hoofdmenuOptionButton">
                             Goederen verkavelen
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='ordenGoederen.php'" class="optionButton">
+                        <button onclick="location.href='ordenGoederen.php'" class="hoofdmenuOptionButton">
                             Goederen ordenen
                         </button>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <button onclick="location.href='opstellenKavellijst.php'" class="optionButton">
+                        <button onclick="location.href='opstellenKavellijst.php'" class="hoofdmenuOptionButton">
                             Kavellijst opstellen
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='toewijzenKavellijst.php'" class="optionButton">
+                        <button onclick="location.href='toewijzenKavellijst.php'" class="hoofdmenuOptionButton">
                             Kavellijst toewijzen aan veiling
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='bekijkKavellijst.php'" class="optionButton">
+                        <button onclick="location.href='bekijkKavellijst.php'" class="hoofdmenuOptionButton">
                             Kavellijst bekijken
                         </button>
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <button onclick="location.href='invoerenVeilingsresultaten.php'" class="optionButton">
+                        <button onclick="location.href='invoerenVeilingsresultaten.php'" class="hoofdmenuOptionButton">
                             Veilingsresultaten invoeren
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='kasboek.php'" class="optionButton">
+                        <button onclick="location.href='kasboek.php'" class="hoofdmenuOptionButton">
                             Kasboek
                         </button>
                     </td>
                     <td>
-                        <button onclick="location.href='logout.php'" class="optionButton">
+                        <button onclick="location.href='logout.php'" class="hoofdmenuOptionButton">
                             Uitloggen
                         </button>
                     </td>
