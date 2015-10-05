b0514bf339040815c9f013d8b15e8a8e763b2a00
confusingSmile <waltervdberg91@hotmail.com>
2015-10-05 16:16:13 +0200

    reviewed my own code
    reviewed what goederen ordenen should do. Customer wants to be able to
    auction goods in an order starting with "#1" so to speak.
    
    Added this functionality

---
+++ sorteerKavellijst.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<?php
+require_once 'vendor/autoload.php';
+    $loader = new Twig_Loader_Filesystem('C:\xampp\htdocs\ProjectVeilinghuis\twig-templates');
+    $twig = new Twig_Environment($loader);
+
+echo $twig->render('sorteerKavellijst.html');
+exit;
